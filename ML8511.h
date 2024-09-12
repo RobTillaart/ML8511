8cc9c29b40f637f079155b21797ff7e519f4aa24
Rob Tillaart <rob.tillaart@gmail.com>
2024-09-12 12:00:58 +0200

    Fix #12, improve interface

--- ML8511.h
+++ ML8511.h
@@ -2,7 +2,7 @@
 //
 //    FILE: ML8511.h
 //  AUTHOR: Rob Tillaart
-// VERSION: 0.1.11
+// VERSION: 0.2.0
 //    DATE: 2020-02-03
 // PURPOSE: ML8511 - UV sensor - library for Arduino
 //     URL: https://github.com/RobTillaart/ML8511
@@ -23,7 +23,7 @@
 
 #include <Arduino.h>
 
-#define ML8511_LIB_VERSION            (F("0.1.11"))
+#define ML8511_LIB_VERSION            (F("0.2.0"))
 
 
 class ML8511
@@ -37,25 +37,29 @@ public:
   //  energyMode = HIGH or LOW;
   //  returns mW per cm2
   float    getUV(uint8_t energyMode = HIGH);
+
   //  for external ADC
+  //  voltage must be >= 1.0 otherwise 0 is returned.
+  //  returns mW per cm2
   float    voltage2mW(float voltage);
 
-  //  voltage must be > 0 otherwise it is not set
-  void     setVoltsPerStep(float voltage, uint32_t steps);
+  //  voltage and steps must both be > 0 otherwise voltagePerStep
+  //  is not set and the function returns false.
+  bool     setVoltsPerStep(float voltage, uint32_t steps);
   float    getVoltsPerStep();
 
-  //  manually enable / disable
-  void     enable();
-  void     disable();
+  //  manually enable / disable the sensor
+  //  if enable pin is not set, device is always enabled.
+  bool     enable();   //  returns false if enable pin not set.
+  bool     disable();  //  returns false if enable pin not set.
   bool     isEnabled();
 
 
-  //  experimental estimate DUV index
+  //  EXPERIMENTAL: estimate DUV index
   //  WARNING: USE WITH CARE
   //
-  //  input in mW per cm2    ==  typical the output of getUV()
+  //  input in mW per cm2 == typical the output of getUV()
   float    estimateDUVindex(float mWcm2);
-
 
   //  https://github.com/RobTillaart/ML8511/issues/4
   //  discusses the calibration

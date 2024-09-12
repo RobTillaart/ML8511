b4640b660cc5a313dc44f7799f218b325ec1466c
Rob Tillaart <rob.tillaart@gmail.com>
2024-09-12 14:12:40 +0200

    Fix build

--- ML8511.h
+++ ML8511.h
@@ -32,34 +32,34 @@ public:
   //  if enablePin is omitted, one must connect EN to 3V3.
   ML8511(uint8_t analogPin, uint8_t enablePin = 0xFF);
 
-  void reset();  //  reset internal variables to initial value.
+  void  reset();  //  reset internal variables to initial value.
 
   //  energyMode = HIGH or LOW;
   //  returns mW per cm2
-  float    getUV(uint8_t energyMode = HIGH);
+  float getUV(uint8_t energyMode = HIGH);
 
   //  for external ADC
   //  voltage must be >= 1.0 otherwise 0 is returned.
   //  returns mW per cm2
-  float    voltage2mW(float voltage);
+  float voltage2mW(float voltage);
 
   //  voltage and steps must both be > 0 otherwise voltagePerStep
   //  is not set and the function returns false.
-  bool     setVoltsPerStep(float voltage, uint32_t steps);
-  float    getVoltsPerStep();
+  bool  setVoltsPerStep(float voltage, uint32_t steps);
+  float getVoltsPerStep();
 
   //  manually enable / disable the sensor
   //  if enable pin is not set, device is always enabled.
-  bool     enable();   //  returns false if enable pin not set.
-  bool     disable();  //  returns false if enable pin not set.
-  bool     isEnabled();
+  bool  enable();   //  returns false if enable pin not set.
+  bool  disable();  //  returns false if enable pin not set.
+  bool  isEnabled();
 
 
   //  EXPERIMENTAL: estimate DUV index
   //  WARNING: USE WITH CARE
   //
   //  input in mW per cm2 == typical the output of getUV()
-  float    estimateDUVindex(float mWcm2);
+  float estimateDUVindex(float mWcm2);
 
   //  https://github.com/RobTillaart/ML8511/issues/4
   //  discusses the calibration
@@ -68,17 +68,17 @@ public:
   //  a value of 1.61 was found to be far more accurate
   //
   //  returns false  if f < 0.01 (to force positive factor only)
-  bool     setDUVfactor(float factor);
-  float    getDUVfactor();
+  bool  setDUVfactor(float factor);
+  float getDUVfactor();
 
 
 private:
-  uint8_t  _analogPin;
-  uint8_t  _enablePin;
-  float    _voltsPerStep;
-  bool     _enabled;
+  uint8_t _analogPin;
+  uint8_t _enablePin;
+  float   _voltsPerStep;
+  bool    _enabled;
 
-  float    _DUVfactor;
+  float   _DUVfactor;
 };
 
 
